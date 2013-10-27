75a70035b7275303dfab07ed7bdb274b6acaf135
DennyScott <DennyScott301@gmail.com>
2013-10-27 03:06:52 -0500

    Added Splash Page

--- client/Concloud.js
+++ client/Concloud.js
@@ -1,3 +1,16 @@
 Template.concloud.rendered = function () {
 	jqueryui();
+
+	 WebFontConfig = {
+    google: { families: [ 'Lato:400,700,900,400italic:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); 
 };
